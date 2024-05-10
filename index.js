bf537b1bf999ae65d55e913b5660a7c8b3431468
Bogdan Nuzhnyy <nuzhnyy@live.dk>
2024-05-10 11:03:33 +0200

    1 commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+
+
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req,res) => {
+
+
+  res.render("index.ejs");
+})
+
+
+
+app.listen (port, () => {
+  console.log(`Server is op and running on port${port}`)
+});
